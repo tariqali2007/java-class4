458191512a7a1f68f885b406d7a297d73fb5aa4a
tariqalimahar <tariqalimahar2007@gmail.com>
2024-05-23 12:26:42 +0500

    class4

---
+++ class.js
@@ -0,0 +1,64 @@
+// function 
+// let nam2=("hello word");
+// function greet (){
+//     console.log("hello students" );
+// }
+// greet()  //function calling 
+// greet()
+// greet()
+// greet() 
+// greet()
+
+
+// function employee (file){ // parameter 
+//     console.log("working on file  " + file );
+// }
+
+// employee("abdullah") // argument 
+// employee("ali ")
+// employee("wasif ")
+// employee("hussan ")
+
+
+// function employee (file){ // parameter 
+//      return "working on file  " + file 
+// }
+
+// let data = employee(7);
+// console.log(data );
+
+// function add (num1 , num2, num3){
+//     return num1 + num2 + num3;
+// }
+
+// let data = add(22, 22 , 33);
+
+// let data2 = add(222, 22 , 333);
+// console.log(data, data2);
+
+
+// closure ==== scope 
+
+// let abc = 123 // global scope 
+// function  print (){
+//     let abc = 123 ; // local scope 
+//     console.log(abc);
+// }
+
+// console.log( abc);  
+// print()
+//  let name =  "Abdullah";
+// function  print () {
+    // console.log(nam2);
+    //global scope
+    //  console.log(name + nam2);
+    // console.log(  name );
+    // return function () {          // anonymous function 
+         // local scope 
+    //     let name = " wasif 123 ";
+    // }
+// }
+
+
+// let innerfunction = print();
+// innerfunction()
